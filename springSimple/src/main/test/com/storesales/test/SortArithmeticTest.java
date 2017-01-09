dfc88f9f7a305098cbfe0d5070ea2692bd31e04f
tanxibiao <tanxibiao@foxmail.com>
2017-01-09 11:03:28 +0800

    增加单元测试

---
+++ springSimple/src/main/test/com/storesales/test/SortArithmeticTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2016 The Etong Tech. Ltd. All rights reserved.
+ * distributed with this file and available online at
+ * http://www.etong.com/
+ */
+
+package com.storesales.test;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by tanxibiao on 2017/1/9.
+ */
+public class SortArithmeticTest extends TestCase {
+
+    @Test
+    public void testOne(){
+        System.out.print("sssssssssssssssssssssss==============");
+    }
+}
